d40ff331a34114ff3a24eb3955065fb83ee06b3e
Sanjarbek113 <matyoqubovsanjarbek81@gmail.com>
2022-02-02 16:05:17 +0500

    add

--- main.js
+++ main.js
@@ -112,4 +112,69 @@ function SetClock(selector, endtime){
 }
 SetClock(".timer", deadline);
 
-     
+
+
+
+window.navigator.geolocation.getCurrentPosition( async mintaqa =>{
+    console.log(mintaqa);
+
+    const latitude = mintaqa.coords.latitude;
+    const lontitude = mintaqa.coords.longitude;
+
+    console.log(latitude, lontitude);
+
+    const response = await fetch(`http://api.aladhan.com/v1/calendar?latitude=${latitude}&longitude=${lontitude}&method=3&month=${new Date().getMonth}&year=${new Date().getFullYear}`);
+    const namoz = await response.json();
+
+    console.log(namoz.data);
+    for(let key of namoz.data){
+        for(let key in namoz.data.timings){
+            console.log(key);
+        }
+    }
+   
+    const SubMit = document.querySelector("#submit");
+    SubMit.addEventListener("click", () =>{
+        const dataOfnamoz = document.querySelector("#kun");
+        let i = parseInt(dataOfnamoz.value ) + 1;
+        if(i < 0){
+            alert("kichik son kititdingiz")
+        } 
+        if( i > 31){
+            alert("katta son kiritdingiz")
+        }
+        else{
+
+            const fajr = namoz.data[i].timings.Fajr;
+            const asr = namoz.data[i].timings.Asr;
+            const Dhuhr = namoz.data[i].timings.Dhuhr;
+            const Imsak = namoz.data[i].timings.Imsak;
+            const Isha = namoz.data[i].timings.Isha;
+            const Maghrib = namoz.data[i].timings.Maghrib;
+            const Midnight = namoz.data[i].timings.Midnight;
+            const Sunrise = namoz.data[i].timings.Sunrise;
+            const Sunset = namoz.data[i].timings.Sunset;
+    
+    
+            
+        const elements = document.querySelectorAll(".p-text");
+        elements[0].textContent = "Imsak :" + Imsak;
+        elements[1].textContent = " Fajr :" + fajr;
+        elements[2].textContent = "Sunrise :" + Sunrise;
+        elements[3].textContent = "Dhuhr :" + Dhuhr;
+        elements[4].textContent = "Asr :" + asr;
+        elements[5].textContent = "Maghrib :" + Maghrib;
+        elements[6].textContent = "Sunset :" + Sunset;
+        elements[7].textContent = "Isha : " + Isha;
+        elements[8].textContent = "Midnight :" + Midnight;
+
+        }
+         
+        
+    
+    });
+   
+
+
+
+})
